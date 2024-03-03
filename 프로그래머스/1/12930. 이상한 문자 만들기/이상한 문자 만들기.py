e9217d26e1a5f26cd5893e0e1a7cd63eefccd22e
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-03-03 18:34:12 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    result = []
+    s = s.split(' ')
+    for sentence in s:
+        answer = ''
+        for idx,sen in enumerate (sentence):
+            if idx % 2 == 0:
+                answer += sen.upper()
+            else :
+                answer += sen.lower()
+        result.append(answer)
+    return ' '.join(result)
