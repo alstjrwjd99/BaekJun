d38111f14e1ee8cf1ff7602d99114da54a3b13a4
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-03-04 10:33:05 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -7,7 +7,6 @@ def solution(id_list, report, k):
         give = tmp[0]
         take = tmp[1]
         reported[id_list.index(give)][id_list.index(take)] = 1 
-    
     for i in range (l_id):
         over = 0
         for j in range (l_id):
@@ -15,9 +14,6 @@ def solution(id_list, report, k):
         if over < k:
             for a in range (l_id):
                 reported[a][i] = 0
-                
     for a in reported:
         answer.append(sum(a))
-    
-    
     return answer
