6c4768bb578daf163851f28a3e01253ac5ea7899
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-03-08 12:21:47 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -2,14 +2,10 @@ def solution(n, arr1, arr2):
     answer = []
     for i in range (n):
         secret = arr1[i] | arr2[i]
-        maps = ''
-        while secret != 0:
-            if secret % 2 == 0:
-                maps += ' '
-            else :
-                maps += '#'
-            secret //= 2
-        maps = maps.ljust(n)
-        maps = maps[::-1]
+        maps = bin(secret)
+        maps = maps[2:]
+        maps = maps.rjust(n,'0')
+        maps = maps.replace('1','#')
+        maps = maps.replace('0',' ')
         answer.append(maps)
     return answer
