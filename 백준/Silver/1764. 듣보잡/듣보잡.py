b4e884fc1e99f74919214e2dea55e547ef0c313b
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-03-19 21:03:13 +0900

    [Silver IV] Title: 듣보잡, Time: 3700 ms, Memory: 44356 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,13 +1,9 @@
-n,m = map(int,input().split())
-answer = []
-name = set()
-for _ in range (n):
-  name.add(input())
-for _ in range (m):
-  nosee = input()
-  if nosee in name:
-    answer.append(nosee)
-answer.sort()
-print(len(answer))
-for a in answer:
-  print(a)
+N,M = map(int,input().split(' ')) 
+a =set([input() for i in range(N)]) #듣지못한사람 set생성
+b=set([input() for i in range(M)]) # 보지못한사람 set생성
+
+c=sorted(list(a & b)) #듣보잡 (set의 교집합 사용)
+
+print(len(c))
+for i in c:
+    print(i)
