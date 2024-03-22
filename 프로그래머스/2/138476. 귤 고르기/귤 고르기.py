80b386d86bb141066b1e295c44ea5c0424bee14a
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-03-22 11:43:54 +0900

    [level 2] Title: 귤 고르기, Time: 25.85 ms, Memory: 18 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -0,0 +1,17 @@
+from collections import Counter
+def solution(k, tangerine):
+    answer = 1
+    counter = Counter(tangerine)
+    values = sorted(list(counter.values()))
+    value_len = len(values)
+
+    max_sum = 0
+    for i in range(value_len - 1, -1, -1): 
+        max_sum+=values[i]
+
+        if max_sum >=k:
+            return answer
+        answer += 1
+
+
+    return answer
