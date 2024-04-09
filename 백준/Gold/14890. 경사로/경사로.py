e1e209715802a07917990e675510c7294a26c415
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-04-09 16:41:44 +0900

    [Gold III] Title: 경사로, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.py
+++ 백준/Gold/14890. 경사로/경사로.py
@@ -1,44 +1,48 @@
+# 입력
 import sys
+input = sys.stdin.readline
+N,L = map(int,input().split())
+maps = []
+for _ in range (N):
+    maps.append(list(map(int,input().split())))
 
-def pos(now):
-    """
-    1. 차이가 1만 경사로 설치 가능
-    2. 현재 높이 < 이전 높이, 경사로 설치를 위해 오른쪽 스캔 (낮은 곳에 경사로 설치)
-    3. 현재 높이 > 이전 높이, 경사로 설치를 위해 왼쪽 스캔 (낮은 곳에 경사로 설치)
-    :param i:
-    :return:
-    """
-    for j in range(1, n):
-        if abs(now[j] - now[j - 1]) > 1:   # 1. 차이가 1만 가능
+answer = 0
+# 지도를 순회하며 내림차순인 경우 or 오름차순(반대에서 건너는 경우)인 경우만 건너갈 수 있음
+# 그 중 경사로를 설치하려면 높이 차이가 1이 나면서 여유 공간 L 만큼이 있어야함
+def line_check(line) :
+    for i in range (1,N):
+        # 차이가 -1, 0, 1 중 하나가 아니면 안됨
+        if abs(line[i] - line[i-1]) > 1:
             return False
-        if now[j] < now[j - 1]:            # 2.  현재 < 이전, 경사로를 만들기 위해 오른쪽을 스캔함(낮은 곳에 경사로 설치)
-            for k in range(l):  # l 만큼 경사로 너비 필요 
-                if j + k >= n or used[j + k] or now[j] != now[j + k]: # 범위 넘어감 or 이미 설치함 or 낮은 곳의 높이가 다른 경우, 그만  
+        if line[i] < line[i-1] :
+            for k in range(L): 
+                # 범위 넘어감 or 이미 설치함 or 낮은 곳의 높이가 다른 경우, 그만  
+                if i + k >= N or used[i + k] or line[i] != line[i + k]:
                     return False
-                if now[j] == now[j + k]:   # 높이가 같은 경우 사용 여부 체크 
-                    used[j + k] = True
-        elif now[j] > now[j - 1]:         # 3.  현재 > 이전, 경사로를 만들기 위해 왼쪽을 스캔함
-            for k in range(l):
-                if j - k - 1 < 0 or now[j - 1] != now[j - k - 1] or used[j - k - 1]:  # 범위 넘어감 or 이미 설치함 or 낮은 곳의 높이가 다른 경우, 그만
+                # 높이가 같은 경우 사용으로 체크 
+                if line[i] == line[i + k]:   
+                    used[i + k] = True
+        # 현재 > 이전, 경사로를 만들기 위해 왼쪽을 스캔함
+        elif line[i] > line[i - 1]:         
+            for k in range(L):
+                # 범위 넘어감 or 이미 설치함 or 낮은 곳의 높이가 다른 경우, 그만
+                if i - k - 1 < 0 or line[i - 1] != line[i - k - 1] or used[i - k - 1]:  
                     return False
-                if now[j - 1] == now[j - k - 1]:   # 높이가 같은 경우 사용 여부 체크 
-                     used[j - k - 1] = True
-    return True   # 모두 문제없이 설치된 경우 가능함을 출력 
+                # 높이가 같은 경우 사용 여부 체크 
+                if line[i - 1] == line[i - k - 1]:   
+                     used[i - k - 1] = True
+    return True
+        
+# 가로 탐색
+for x in range (N):
+    used = [False for _ in range (N)]
+    if line_check(maps[x]):
+        answer += 1
+    
+# 세로 탐색
+for x in range (N):
+    used = [False for _ in range (N)]
+    if line_check([maps[i][x] for i in range (N)]):
+        answer += 1
 
-n, l = map(int, sys.stdin.readline().split())
-graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-result = 0
-
-# 1. 가로 확인
-for i in range(n):
-    used = [False for _ in range(n)]  # 사용 여부
-    if pos(graph[i]):  # 현재 확인할 길을 넣어준다.
-        result += 1
-
-# 2. 세로 확인
-for i in range(n):
-    used = [False for _ in range(n)]
-    if pos([graph[j][i] for j in range(n)]):  # 현재 확인할 길을 넣어준다.
-        result += 1
-
-print(result)
+print(answer)
