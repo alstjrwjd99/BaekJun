8bcc7c106f5b0583e9059f10bf86eba19a3ff018
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-06-14 13:29:02 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,26 @@
+def solution(begin, target, words):
+    # 못찾는 경우
+    if target not in set(words):
+        return 0
+    answer = 0
+    stack = [(begin,0)]
+    visited = set()
+    visited.add(begin)
+    while stack:
+        start,cnt = stack.pop()        
+        for word in words:
+            if word not in visited:
+                wrong_cnt = 0
+                is_diff_one = True
+                for i,j in zip(start, word):
+                    if i != j:
+                        wrong_cnt += 1
+                    if wrong_cnt > 1:
+                        is_diff_one = False
+                        break
+                if is_diff_one:
+                    if word == target:
+                        answer = cnt + 1
+                    stack.append((word,cnt + 1))
+                    visited.add(word)
+    return answer
