319d8556f79acbca5e40249fb506f08df0ad1676
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-07-29 13:57:52 +0900

    [level 3] Title: 선입 선출 스케줄링, Time: 12.33 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.py
+++ 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.py
@@ -1,26 +1,27 @@
 def solution(n, cores):
-    if n<=len(cores):
-        return n
 
-    n -= len(cores)
-    left,right = 1, max(cores) * n
-    
-    while left < right:
-        mid = (left + right)//2
-        task = 0
-        for core in cores:
-            task += mid // core 
-        
-        if task >= n :
-            right = mid
-        else :
-            left = mid + 1
-    
-    for core in cores:
-        n -= (right-1) // core
-    
-    for i in range(len(cores)):
-        if right % cores[i] == 0:
-            n -= 1
-            if n == 0:
-                return i + 1
+    if n <= len(cores):
+        return n
+    else:
+        n -= len(cores)
+        left = 1
+        right = max(cores) * n
+
+        while left < right:
+            mid = (left + right) // 2
+            capacity = 0
+            for c in cores:
+                capacity += mid // c
+            if capacity >= n:
+                right = mid
+            else:
+                left = mid + 1
+
+        for c in cores:
+            n -= (right-1) // c
+
+        for i in range(len(cores)):
+            if right % cores[i] == 0:
+                n -= 1
+                if n == 0:
+                    return i + 1
