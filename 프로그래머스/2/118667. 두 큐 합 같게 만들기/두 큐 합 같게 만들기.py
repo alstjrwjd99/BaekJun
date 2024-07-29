60e626ef4f81c1625ebc772de0bf6869e1e7ae2a
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-07-29 21:47:05 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 230.03 ms, Memory: 19.2 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -2,30 +2,29 @@ from collections import deque
 
 def solution(q1, q2):
     answer = -1
+
+    ql = [deque(q1), deque(q2)]
+    sl = [sum(q1), sum(q2)]
     
-    q1 = deque(q1)
-    q2 = deque(q2)
-    s1 = sum(q1)
-    s2 = sum(q2)
-
+    # 홀수인 경우
+    if sum(sl) % 2 != 0:
+        return -1
+    
     count = 0
-    l = 2 * sum([len(q1), len(q2)])
+    # l = 2 * sum([ len(v) for v in ql ])
+    l = 2 * sum(map(len,ql))
     while count < l:
-        if s1 < s2:
-            c2 = q2.popleft()
-            q1.append(c2)
-
-            s2 -= c2
-            s1 += c2
-        elif s1 > s2:
-            c1 = q1.popleft()
-            q2.append(c1)
-
-            s1 -= c1
-            s2 += c1
-        else:
+        if sl[0] == sl[1]:
             answer = count
             break
+
+        idx = 1 if sl[0] < sl[1] else 0
+
+        c = ql[idx].popleft()
+        ql[1-idx].append(c)
+
+        sl[idx] -= c
+        sl[1-idx] += c
         count += 1
     
     return answer
