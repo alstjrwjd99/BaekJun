8b8a1fb0b30c8f8ecddde787fb061d1df35080f7
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-07-31 12:43:43 +0900

    [level 4] Title: 도둑질, Time: 819.41 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -1,13 +1,22 @@
 def solution(money):
-    dp = [0] * len(money)
-    ddp = [0] * len(money)
-    
-    dp[0] = money[0]
-    dp[1] = money[0]
-    ddp[0] = 0
-    ddp[1] = money[1]
-    
-    for i in range (2, len(money)):
-        dp[i] = max(dp[i-1],dp[i-2] + money[i])
-        ddp[i] = max(ddp[i-1],ddp[i-2] + money[i])
-    return max(dp[-2],ddp[-1])
+    answer = 0
+
+    dp1 = [0] * len(money) # 첫 집을 털었을 때
+    dp2 = [0] * len(money) # 첫 집을 안 털고 두 번째 집을 털었을 때
+    dp3 = [0] * len(money) # 첫 집과 두 번째 집을 안 털었을 때
+
+    dp1[0] = money[0]
+    dp1[1] = dp1[0]
+    dp2[1] = money[1]
+
+    for i in range(2, len(money) - 1):
+        dp1[i] = max(dp1[i - 1], dp1[i - 2] + money[i])
+        dp2[i] = max(dp2[i - 1], dp2[i - 2] + money[i])
+        dp3[i] = max(dp3[i - 1], dp3[i - 2] + money[i])
+
+    dp1[-1] = dp1[-2]
+    dp2[-1] = max(dp2[-2], dp2[-3] + money[-1])
+    dp3[-1] = max(dp3[-2], dp3[-3] + money[-1])
+    answer = max(dp1[-1], dp2[-1], dp3[-1])
+
+    return answer
