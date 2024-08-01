4099becdee5f45e228fec4a8a33810e6282afd47
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-08-01 21:47:34 +0900

    [level 2] Title: 가장 큰 수, Time: 1080.67 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,12 +1,11 @@
 from functools import cmp_to_key
 
 def solution(numbers):
-    answer = ''
     # 두 숫자를 바꿔야하는지 여부
     def to_swap(x,y):
         if str(y)+str(x)>=str(x)+str(y) : return 1
         else : return -1
     numbers.sort(key = cmp_to_key(to_swap))
-    return str(int(''.join(map(str,numbers)))) # [0,0]이 주어지는 경우 대비해서 str(int())
-
+    answer = ''.join(map(str,numbers))
+    return "0" if answer[0] == "0" else answer
                  
