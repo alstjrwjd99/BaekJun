ed1a961fc7bca1ff363313456a41fc35f1231208
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-09-02 09:57:42 +0900

    [Gold V] Title: LCS, Time: 300 ms, Memory: 55708 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+a = input().strip()
+b = input().strip()
+
+def lcs(a, b):
+    len_a = len(a)
+    len_b = len(b)
+    
+    dp = [[0] * (len_b + 1) for _ in range(len_a + 1)]
+    
+    for i in range(1, len_a + 1):
+        for j in range(1, len_b + 1):
+          if a[i-1] == b[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+          else :
+            dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+    return dp[len_a][len_b]
+
+print(lcs(a, b))
