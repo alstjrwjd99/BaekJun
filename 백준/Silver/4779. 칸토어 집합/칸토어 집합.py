66ba181d480488a49abc0e0b1a15d9b813864732
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-09-03 09:31:52 +0900

    [Silver III] Title: 칸토어 집합, Time: 56 ms, Memory: 36652 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.read
+
+numbers = input().splitlines()
+
+def kantor(n, idx):
+    if n == 1:
+        line[idx] = '-'
+        return
+    
+    # 현재 구간의 길이와 중간 구간의 시작 위치를 계산
+    segment_length = n // 3
+    
+    # 왼쪽과 오른쪽 구간을 '-'로 설정
+    kantor(segment_length, idx)
+    kantor(segment_length, idx + 2 * segment_length)
+
+
+for n in numbers:
+    if n.strip() == '':  # 빈 줄을 처리
+        continue
+    
+    n = int(n)
+    length = 3**n
+    line = [' '] * length
+
+    kantor(length, 0)
+    print(''.join(line))
