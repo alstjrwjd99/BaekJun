52670c32e817bcbb47ba196484d769109be87215
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-09-04 14:29:35 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 108 ms, Memory: 39212 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -1,11 +1,18 @@
-size = list(map(int,input().split()))
-arr = []
-for _ in range (size[0]):
-  arr.append(list(map(int,input().split())))
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
 k = int(input())
-for _ in range (k):
-  answer = 0
-  scope = list(map(int,input().split()))
-  for i in range (scope[0]-1,scope[2]):
-    answer += sum(arr[i][scope[1]-1:scope[3]])
-  print(answer)
+scopes = [list(map(int, input().split())) for _ in range(k)]
+
+# 2차원 누적 합 배열 계산
+prefix_sum = [[0] * (M + 1) for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    for j in range(1, M + 1):
+        prefix_sum[i][j] = arr[i-1][j-1] + prefix_sum[i-1][j] + prefix_sum[i][j-1] - prefix_sum[i-1][j-1]
+
+for x1, y1, x2, y2 in scopes:
+    result = prefix_sum[x2][y2] - prefix_sum[x1-1][y2] - prefix_sum[x2][y1-1] + prefix_sum[x1-1][y1-1]
+    print(result)
