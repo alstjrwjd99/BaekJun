c07db2c864d066613008113db1633bcec622df22
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-09-06 11:34:01 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 468 ms, Memory: 240844 KB -BaekjoonHub

---
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+S = input().strip()
+made = set()
+
+for i in range (1,len(S)):
+    for j in range (len(S)-i +1 ):
+        made.add(S[j:j+i])
+
+print(len(made) + 1)
