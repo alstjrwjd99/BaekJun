4f3162c2a73967b6c153bd20b570e733f1bd66e5
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-10-13 15:23:00 +0900

    [level 3] Title: 베스트앨범, Time: 0.07 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,24 +1,20 @@
+from collections import defaultdict
+
 def solution(genres, plays):
     answer = []
-    streaming = dict()
-    each_played = dict()
-    for idx,(i,j) in enumerate(zip(genres,plays)):
-        if i not in streaming:
-            each_played[i] = [(idx,j)]
-            streaming[i] = j
-        else :
-            each_played[i].append((idx,j))
-            streaming[i] += j
-
-    chart = []
-    for playtime in streaming:
-        chart.append((streaming[playtime],playtime))
-    chart.sort(reverse = True)
-    for gen in chart:
-        if len(each_played[gen[1]]) == 1:
-            answer.append(each_played[gen[1]][0][0])
-        else:
-            each_played[gen[1]].sort(key = lambda x : (-x[1],x[0]))
-            answer.append(each_played[gen[1]][0][0])
-            answer.append(each_played[gen[1]][1][0])
+    genre_by_play = defaultdict(int)
+    genre_by_idx = defaultdict(list)
+    
+    for idx, (genre, play) in enumerate(zip(genres, plays)):
+        genre_by_play[genre] += play
+        genre_by_idx[genre].append((play,idx))
+        
+    genre_ranking = sorted(genre_by_play.keys(), key=lambda g: genre_by_play[g], reverse=True)
+    
+    for genre in genre_ranking:
+        target_list = sorted(genre_by_idx[genre], key=lambda x : (-x[0],x[1]))
+        answer.append(target_list[0][1])
+        if len(target_list) > 1:
+            answer.append(target_list[1][1])
+            
     return answer
