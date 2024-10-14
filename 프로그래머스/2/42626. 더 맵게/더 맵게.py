34102fa0c00ebe6417447757d301f36eee57ebb6
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-10-14 09:30:54 +0900

    [level 2] Title: 더 맵게, Time: 1737.20 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -2,12 +2,15 @@ import heapq
 
 def solution(scoville, K):
     answer = 0
-    heapq.heapify(scoville)
-    while scoville[0] < K:
-        least = heapq.heappop(scoville)
-        second = heapq.heappop(scoville)
-        heapq.heappush(scoville, least + second * 2)
+    hq = []
+    for degree in scoville:
+        heapq.heappush(hq,degree)
+        
+    while len(hq)>1 and hq[0] < K :
+        one = heapq.heappop(hq)
+        two = heapq.heappop(hq)
+        heapq.heappush(hq, (one + two * 2))
         answer += 1
-        if len(scoville) < 2 and scoville[0] < K:
-            return -1
+    
+    if hq[0] < K : return -1
     return answer
