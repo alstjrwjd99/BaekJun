70df6f9072d7b9b0307613ce70da71b47d3ff748
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-10-14 11:15:44 +0900

    [level 3] Title: 입국심사, Time: 653.09 ms, Memory: 14.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 14.3 MB, 시간: 577.51 ms
+메모리: 14.2 MB, 시간: 653.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 03일 12:46:22
+2024년 10월 14일 11:15:41
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,17 +1,21 @@
 def solution(n, times):
-    left = 1
-    right = max(times) * n
+    left, right = 1, max(times) * n  # 최소 1분, 최대 시간은 가장 긴 심사관이 모두 처리할 경우
+    answer = right
     
-    while left < right:
-        mid = left + (right - left) // 2
-        done = 0
+    while left <= right:
+        mid = (left + right) // 2
+        judge_member = 0
         
+        # mid 시간 동안 심사할 수 있는 인원 수 계산
         for time in times:
-            done += mid // time
+            judge_member += mid // time
         
-        if done >= n:
-            right = mid
+        if judge_member >= n:
+            # 가능한 경우, 더 작은 시간을 찾아본다
+            answer = mid
+            right = mid - 1
         else:
+            # 심사 인원이 부족한 경우, 시간을 더 늘려야 한다
             left = mid + 1
     
-    return left
+    return answer
