8a0ec08ab7a8ac7d2f09bd1e3e04ff1d33599089
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-10-14 10:19:57 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,9 +1,10 @@
 def solution(cards1, cards2, goal):
-    for g in goal:
-        if len(cards1) > 0 and g == cards1[0]:
-            cards1.pop(0)       
-        elif len(cards2) >0 and g == cards2[0]:
+    while goal:
+        goal_card = goal.pop(0)
+        # 2개가 같다면? -> 다른 단어만 존재한다네요
+        if cards1 and goal_card == cards1[0]:
+            cards1.pop(0)
+        elif cards2 and goal_card == cards2[0]:
             cards2.pop(0)
-        else:
-            return "No"
-    return "Yes"
+        else : return 'No'
+    return 'Yes'
