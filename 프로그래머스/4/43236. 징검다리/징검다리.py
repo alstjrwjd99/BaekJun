8bb2b95a32ce072539fc361d8a5b9c5a126dbff4
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-10-14 11:47:58 +0900

    [level 4] Title: 징검다리, Time: 138.52 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,25 +1,26 @@
+'''
+찾아야하는 것 : 바위 거리의 최솟 값 중 가장 큰 값
+바위 거리를 mid로 두고 찾기
+'''
+
 def solution(distance, rocks, n):
     rocks.sort()
-    left = 1
-    right = distance
     rocks.append(distance)
-    
-    while left <= right:
-        mid = (left + (right-left)//2)
-        prev = 0
+    left, right = 0, distance
+    while left <= right :
+        mid = (left + right) // 2
         removed = 0
-        
+        prev = 0
         for rock in rocks:
             if rock - prev < mid:
                 removed += 1
-            else:
+            else :
                 prev = rock
             if removed > n:
                 break
-
-        if removed > n:
+        if removed > n :
             right = mid - 1
-        else:
+        else :
             left = mid + 1
-
-    return left-1
+        
+    return left - 1
