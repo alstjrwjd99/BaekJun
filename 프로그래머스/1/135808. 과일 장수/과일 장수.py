0fd631cd7a100e034bef8ad92b59fdb6c8987ef5
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-10-15 10:50:19 +0900

    [level 1] Title: 과일 장수, Time: 88.93 ms, Memory: 21.6 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,10 +1,6 @@
 def solution(k, m, score):
     answer = 0
-    score.sort()
-    while len(score) >= m:
-        for i in range (m):
-            apple = score.pop()
-            if i == m-1:
-                less = apple
-        answer += less * m
+    score.sort(reverse = True)
+    for i in range (m-1,len(score),m):
+        answer += score[i] * m
     return answer
