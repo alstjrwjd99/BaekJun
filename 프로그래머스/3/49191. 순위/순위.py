fc3aef9c2b119f918d069c36ccdb2d150f619cee
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-10-17 11:24:47 +0900

    [level 3] Title: 순위, Time: 133.24 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,31 +1,20 @@
-from collections import deque
-
 def solution(n, results):
     answer = 0
-    left = {i+1 : [] for i in range(n)}
-    right = {i+1 : [] for i in range(n)}
+    vtx =[[''] * (n+1) for _ in range (n+1)]
+    for win, lose in results:
+        vtx[win][lose] = 'win'
+        vtx[lose][win] = 'lose'
     
-    for result in results:
-        left[result[0]].append(result[1])
-        right[result[1]].append(result[0])
+    for k in range (n+1):
+        for i in range (n+1):
+            for j in range (n+1):
+                if vtx[i][k] == vtx[k][j] != '':
+                    vtx[i][j] = vtx[i][k]
     
-    def bfs(start,lr):
-        queue = deque([start])
-        visited =set()
-        while queue:
-            gwon = queue.popleft()
-            for i in lr[gwon]:
-                if i not in visited:
-                    queue.append(i)
-                    visited.add(i)
-        return visited
-        
-        
-    for i in range(1,n+1):
-        left_mem = bfs(i,left)
-        right_mem = bfs(i,right)
-        total = left_mem | right_mem
-        if len(total) == n-1:
-            answer += 1
-    
+    for i in range (1,n+1):
+        can_rank = True
+        for j in range (1,n+1):
+            if i == j : continue
+            if vtx[i][j] == '': can_rank = False
+        if can_rank : answer += 1
     return answer
