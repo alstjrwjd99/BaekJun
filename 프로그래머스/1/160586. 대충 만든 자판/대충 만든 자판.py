a3be57d38552be04b5ab89bbb0d46ead8e87a0d8
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-10-17 12:44:33 +0900

    [level 1] Title: 대충 만든 자판, Time: 2.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,18 +1,24 @@
+from collections import defaultdict
+
 def solution(keymap, targets):
+    # 문자 별 최소 누름 횟수 기록
+    min_presses = defaultdict(lambda: 1e9)  # 충분히 큰 값으로 초기화
+    
+    for key in keymap:
+        for idx, s in enumerate(key):
+            min_presses[s] = min(min_presses[s], idx + 1)
+    
     answer = []
+    
+    # 타겟 문자열 처리
     for target in targets:
-        ans = 0
-        for a in target:
-            isFind = False
-            press = 101
-            for key in keymap:
-                if a in key:
-                    press = min(press,key.index(a)+1)
-                    isFind = True
-            if not isFind:
-                ans = -1
+        tmp = 0
+        for char in target:
+            if min_presses[char] == 1e9:  # 해당 문자가 keymap에 없는 경우
+                tmp = -1
                 break
-            ans += press
-            
-        answer.append(ans)
+            else:
+                tmp += min_presses[char]
+        answer.append(tmp)
+    
     return answer
