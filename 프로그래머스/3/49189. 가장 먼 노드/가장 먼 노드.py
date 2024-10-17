098fa108025079edff2dc000a209f7229b3a3c96
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-10-17 12:04:18 +0900

    [level 3] Title: 가장 먼 노드, Time: 45.29 ms, Memory: 23.8 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,21 +1,27 @@
-from collections import deque
+from collections import defaultdict, deque
 
 def solution(n, edge):
-    answer = [0] * (n+1)
-    graph = {i:[] for i in range (1,n+1)}
-    for a,b in edge:
-        graph[a].append(b)
-        graph[b].append(a)
-    queue = deque([(1,0)])
-    visited = set([1])
-    while queue:
-        node,distance = queue.popleft()
-        for next_node in graph[node]:
-            if not next_node in visited:
-                queue.append((next_node,distance+1))
-                visited.add(next_node)
-                # leaf node
-                if graph[next_node]:
-                    answer[next_node] = distance+1
+    # 인접 리스트로 그래프 생성
+    graph = defaultdict(list)
+    for u, v in edge:
+        graph[u].append(v)
+        graph[v].append(u)
     
-    return answer.count(max(answer))                    
+    # BFS를 위한 큐와 방문 집합
+    queue = deque([(1, 0)])
+    visited = set([1])
+    distances = defaultdict(list)
+    
+    while queue:
+        node, cnt = queue.popleft()
+        
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                queue.append((neighbor, cnt + 1))
+                visited.add(neighbor)
+                distances[cnt + 1].append(neighbor)
+    
+    # 가장 먼 거리의 노드 개수 반환
+    answer = len(distances[max(distances.keys())])
+
+    return answer
