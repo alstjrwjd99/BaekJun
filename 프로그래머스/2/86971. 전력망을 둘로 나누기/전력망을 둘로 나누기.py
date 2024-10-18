1d93817ae205411b7b22300eae1442f8182345fc
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-10-18 14:13:37 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 8.62 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,23 +1,37 @@
-from collections import deque
-
-def count_tree(n,graph):
-    queue = deque([graph[0][0],graph[0][1]])
-    visited = set()
-    while queue:
-        point = queue.popleft()
-        for edge in graph:
-            if edge[0] == point and edge[1] not in visited:
-                queue.append(edge[1])
-                visited.add(edge[1])
-            elif edge[1] == point and edge[0] not in visited:
-                queue.append(edge[0])
-                visited.add(edge[0])
-                
-    return abs(n-len(visited)-len(visited))
+from collections import deque, defaultdict
 
 def solution(n, wires):
-    answer = n
-    for i in range (n-1):
-        seperated = wires[:i] + (wires[i+1:])
-        answer = min(answer,count_tree(n,seperated))
+    answer = float('inf')
+    
+    def find(wires):
+        visited = set()
+        graph = defaultdict(list)
+        group = []
+        
+        for w,i in wires :
+            graph[w].append(i)
+            graph[i].append(w)
+        
+        def bfs(i):
+            queue = deque([i])
+            visited.add(i)
+            tmp = [i]
+            while queue:
+                node= queue.popleft()
+                for nex in graph[node]:
+                    if nex not in visited:
+                        queue.append(nex)
+                        visited.add(nex)
+                        tmp.append(nex)
+            return len(tmp)
+            
+        
+        for i in range (1,n+1):
+            if i not in visited :
+                group.append(bfs(i))
+            
+        return abs(group[0] - group[1])
+        
+    for i in range (len(wires)):
+        answer = min(answer,find(wires[:i] + wires[i+1:]))
     return answer
