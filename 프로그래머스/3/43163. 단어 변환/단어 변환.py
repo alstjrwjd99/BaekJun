b9b8196cb21110efc5b5e68c7e82c0be1fd0b75f
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-10-18 11:47:09 +0900

    [level 3] Title: 단어 변환, Time: 0.42 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,25 +1,16 @@
+from collections import deque
+
 def solution(begin, target, words):
-    # 못찾는 경우
-    if target not in set(words):
-        return 0
-    answer = 0
-    stack = [(begin,0)]
+    if target not in set(words): return 0
     visited = set()
-    visited.add(begin)
-    while stack:
-        start,cnt = stack.pop()        
-        for word in words:
-            if word not in visited:
-                wrong_cnt = 0
-                is_diff_one = True
-                for i,j in zip(start, word):
-                    if i != j:
-                        wrong_cnt += 1
-                    if wrong_cnt > 1:
-                        is_diff_one = False
-                        break
-                if is_diff_one:
-                    if word == target:
-                        return cnt + 1
-                    stack.append((word,cnt + 1))
-                    visited.add(word)
+    queue = deque([(begin,0)])
+    while queue:
+        word, cnt = queue.popleft()
+        if word== target:return cnt
+        for w in words:
+            dif = 0
+            for idx, s in enumerate(word):
+                if s != w[idx] : dif += 1
+            if dif == 1 and w not in visited:
+                queue.append((w,cnt+1))
+                visited.add(w)
