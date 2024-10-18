785637d61635c23b29e0af2dfe3c7522619573e8
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-10-18 14:45:46 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.15 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
@@ -1,25 +1,21 @@
 def solution(cards):
-    opened = [False] * len(cards)
-    box_group = []
+    answer = []
+    cards = [0] + cards
+    visited = set([0])
     
-    def open_box(cards, idx):
-        opened[idx] = True
-        tmp = [idx]    
-        while True:
-            next_open = cards[idx] - 1
-            if opened[next_open]:
-                break
-            opened[next_open] = True
-            idx = next_open
-            tmp.append(idx)
-        box_group.append(tmp)
-        
-    for i in range (len(cards)):
-        if opened[i]:
-            continue
-        open_box(cards,i)
+    def find_cycle(num):
+        visited.add(num)
+        tmp = []
+        while num not in tmp:
+            visited.add(cards[num])
+            tmp.append(num)
+            num = cards[num]
+        return tmp
     
-    box_group.sort(key=lambda x : -len(x))
-    if len(box_group) == 1:
-        return 0
-    return len(box_group[0]) * len(box_group[1])
+    for i in range (1,len(cards)):
+        if i not in visited:
+            answer.append(find_cycle(i))
+            
+    if len(answer) == 1: return 0
+    answer.sort(key = lambda x : -len(x))
+    return len(answer[0]) * len(answer[1])
