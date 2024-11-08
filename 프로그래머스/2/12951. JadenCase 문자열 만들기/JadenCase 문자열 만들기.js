13bd206a2782e9dec7acc13b8725fdc65e1c609c
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-11-08 13:42:24 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,11 @@
+function solution(s) {
+    var answer = '';
+    let result = s.split(' ');
+
+    let result2 = result.map(x=> {
+            return x.charAt(0).toUpperCase() + x.slice(1).toLowerCase()    
+    });
+
+    answer = result2.join(" ")
+    return answer;
+}
