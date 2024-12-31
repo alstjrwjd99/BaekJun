b3000e2a0bf0163ad6ada86d307551dae41e784c
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2024-12-31 11:58:18 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 1564.67 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -2,24 +2,21 @@ from itertools import product
 
 def solution(users, emoticons):
     max_subscribers, max_revenue = 0, 0
-    
     n = len(emoticons)
-    discount_rates = [10, 20, 30, 40]
-    for discounts in product(discount_rates, repeat=n):
-        subscribers, revenue = 0, 0
-        
+    discount_rate = [10,20,30,40]
+    
+    for discounts in product(discount_rate, repeat = n):
+        subscribers, revenue = 0,0
         for min_discount, max_price in users:
             total_cost = 0
             for emoticon_price, discount_rate in zip(emoticons, discounts):
                 if discount_rate >= min_discount:
-                    total_cost += emoticon_price * (100 - discount_rate) / 100
-                    
-            if total_cost >= max_price:
-                subscribers += 1
-            else:
-                revenue += total_cost
+                    total_cost += emoticon_price / 100 * (100 - discount_rate )
         
-        if subscribers > max_subscribers or (subscribers == max_subscribers and revenue > max_revenue):
-            max_subscribers, max_revenue = subscribers, revenue
-
-    return [max_subscribers, max_revenue]
+            if total_cost >= max_price :
+                subscribers += 1
+            else : revenue += total_cost
+    
+        if subscribers > max_subscribers or (subscribers == max_subscribers and max_revenue < revenue) :
+            max_subscribers , max_revenue = subscribers  , revenue
+    return [max_subscribers , max_revenue]
