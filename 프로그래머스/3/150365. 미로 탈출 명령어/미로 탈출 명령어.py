1e4bc0a2441162a17e9d96c843a8273ed69d0dde
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2025-01-06 11:27:10 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
@@ -1,34 +1,39 @@
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**8)
-
-dx = [1, 0, 0, -1]
-dy = [0, -1, 1, 0]
-dAlpha = ['d', 'l', 'r', 'u']
-answer = "z"
-
-
-def isVaild(nx, ny, n, m):
-    return 1 <= nx and nx <= n and 1 <= ny and ny <= m
-
-
-def dfs(n, m, x, y, r, c, prev_s, cnt, k):
-    global answer
-    if k < cnt + abs(x - r) + abs(y - c):
-        return
-    if x == r and y == c and cnt == k:
-        answer = prev_s
-        return
-    for i in range(4):
-        if isVaild(x + dx[i], y + dy[i], n, m) and prev_s < answer:
-            dfs(n, m, x + dx[i], y + dy[i], r, c, prev_s+dAlpha[i], cnt + 1, k)
-
-
 def solution(n, m, x, y, r, c, k):
-    dist = abs(x - r) + abs(y - c)
-    if dist > k or (k - dist) % 2 == 1:
+    # 좌상단이 (1, 1), 우하단이 (n, m)이고 초기 위치는 (x, y) -> 목적지 (r, c)
+    diff = abs(x - r) + abs(y - c)
+    
+    # 이동이 불가능한 경우
+    if diff > k or (diff % 2 != k % 2):
         return "impossible"
 
-    dfs(n, m, x, y, r, c, "", 0, k)
+    # dlru 순으로 이동량 계산
+    rest = k - diff
+    dcount, ucount, lcount, rcount = 0, 0, 0, 0
+
+    if x < r:
+        dcount = r - x  # 아래로 내려가기
+    else:
+        ucount = x - r  # 위로 올라가기
+
+    if y < c:
+        rcount = c - y  # 오른쪽으로 이동
+    else:
+        lcount = y - c  # 왼쪽으로 이동
+
+    # 추가 이동량 분배
+    dplus = min(n - max(x, r), rest // 2)  # 남은 아래로 이동 가능한 범위
+    rest -= dplus * 2
+
+    lplus = min(min(y, c) - 1, rest // 2)  # 남은 왼쪽으로 이동 가능한 범위
+    rest -= lplus * 2
+
+    # 경로 생성
+    answer = (
+        "d" * (dcount + dplus) +
+        "l" * (lcount + lplus) +
+        "rl" * (rest // 2) +
+        "r" * (rcount + lplus) +
+        "u" * (ucount + dplus)
+    )
 
     return answer
