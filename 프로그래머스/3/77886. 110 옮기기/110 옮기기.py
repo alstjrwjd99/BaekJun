21cb28a4b4710c8e84415e1aac4a4899c38c8bdc
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2025-01-08 14:24:25 +0900

    [level 3] Title: 110 옮기기, Time: 465.76 ms, Memory: 14.6 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.py
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.py
@@ -1,18 +1,19 @@
-def solution(s):
-    def reorder(x):
-        count_110 = 0
+def solution(s):   
+    def find_110(x):
         stack = []
-
+        cnt_110 = 0
+        
         for char in x:
             stack.append(char)
-            if len(stack) >= 3 and stack[-3:] == ['1', '1', '0']:
+            if len(stack) >= 3 and stack[-3:] == ['1','1','0']:
                 stack.pop()
                 stack.pop()
                 stack.pop()
-                count_110 += 1
-                
-        result = ''.join(stack)
-        idx = result.rfind('0') + 1
-        return result[:idx] + '110' * count_110 + result[idx:]
-
-    return [reorder(x) for x in s]
+                cnt_110 += 1
+        
+        new_x = ''.join(stack)
+        idx = new_x.rfind('0') + 1
+        return new_x[:idx] + '110' * cnt_110 + new_x[idx:]
+        
+        
+    return [find_110(x) for x in s]
