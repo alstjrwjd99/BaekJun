673f7b16872ee0c377ebb02c4c0fbcfe6de907cf
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2025-01-23 17:12:18 +0900

    [Silver I] Title: 경로 찾기, Time: 188 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,29 +1,19 @@
-import sys
-input = sys.stdin.readline
-
-from collections import deque
-
 N = int(input())
-vtx = [
-    list(map(int,input().split()))
-    for _ in range (N)
-]
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, input().split())))
+    
+    
+#플로이드-워셜 알고리즘
+for k in range(N):
+    for i in range(N):
+        for j in range(N): 
+            if graph[i][j] == 1 or (graph[i][k] == 1 and graph[k][j] == 1):
+                graph[i][j] = 1
 
-ans = [[0] * N for _ in range (N)]
 
-q = deque([])
-
-for i in range (N):
-    for j in range (N):
-        if vtx[i][j] == 1:
-            q.append(j)
-            ans[i][j] = 1
-            while q:
-                connect = q.popleft()
-                for idx in range (N):
-                    if vtx[connect][idx] == 1 and ans[i][idx] == 0:
-                        q.append(idx)
-                        ans[i][idx] = 1
-
-for a in ans:
-    print(*a)
+#출력
+for row in graph:
+    for col in row:
+        print(col, end = " ")
+    print()
