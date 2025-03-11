5e98f5912fe702aafd8e4c8cdc4c27a1beb86fb9
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2025-03-11 14:04:51 +0900

    [Silver I] Title: 1로 만들기 2, Time: 1308 ms, Memory: 184020 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -3,33 +3,28 @@ input = sys.stdin.readline
 
 n = int(input().strip())
 
-class X:
-    def __init__(self):
-        self.value = 1e9
-        self.prev = -1  # 이전 위치를 저장해 경로를 추적
-
-dp = [X() for _ in range(n + 1)]
-dp[n].value = 0  # 시작점 설정
+dp = [[1e9, 0] for _ in range(n + 1)]
+dp[n][0] = 0
+dp[n][1] = -1
 
 for i in range(n, 0, -1):
-    if i % 3 == 0 and dp[i // 3].value > dp[i].value + 1:
-        dp[i // 3].value = dp[i].value + 1
-        dp[i // 3].prev = i  # 이전 위치 기록
+    if i % 3 == 0 and dp[i // 3][0] > dp[i][0] + 1:
+        dp[i // 3][0] = dp[i][0] + 1
+        dp[i // 3][1] = i 
 
-    if i % 2 == 0 and dp[i // 2].value > dp[i].value + 1:
-        dp[i // 2].value = dp[i].value + 1
-        dp[i // 2].prev = i  # 이전 위치 기록
+    if i % 2 == 0 and dp[i // 2][0] > dp[i][0] + 1:
+        dp[i // 2][0] = dp[i][0] + 1
+        dp[i // 2][1] = i 
 
-    if dp[i - 1].value > dp[i].value + 1:
-        dp[i - 1].value = dp[i].value + 1
-        dp[i - 1].prev = i  # 이전 위치 기록
+    if dp[i - 1][0] > dp[i][0] + 1:
+        dp[i - 1][0] = dp[i][0] + 1
+        dp[i - 1][1] = i  
 
-# 경로 복원 (Backtracking)
 path = []
 cur = 1
 while cur != -1:
     path.append(cur)
-    cur = dp[cur].prev
+    cur = dp[cur][1]
 
-print(dp[1].value)
-print(*reversed(path))  # 경로를 올바른 순서로 출력
+print(dp[1][0])
+print(*reversed(path))
