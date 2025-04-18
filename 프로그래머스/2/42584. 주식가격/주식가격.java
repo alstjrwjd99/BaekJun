c3ba1475ae9305b4dddda609a0f73874cb8a8b96
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2025-04-18 15:30:49 +0900

    [level 2] Title: 주식가격, Time: 30.88 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,24 +1,38 @@
+/*
+1. 스택에 맨위보다 높은 주식 (가격, 인덱스) 담기
+2. 맨위보다 낮은 가격을 만날 때는 while 문을 쓰면서 pop하고 answer 갱신
+*/
+import java.util.*;
+
 import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int n = prices.length;
-        int[] answer = new int[n];
-        Deque<int[]> stack = new ArrayDeque<>();
-        
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && stack.peek()[0] > prices[i]) {
-                int[] top = stack.pop();
-                answer[top[1]] = i - top[1];
+        int[] answer = new int[prices.length];
+        Deque<Stock> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[i] < stack.peek().price) {
+                Stock top = stack.pop();
+                answer[top.idx] = i - top.idx;
             }
-            stack.push(new int[]{prices[i], i});
+            stack.push(new Stock(prices[i], i));
         }
-        
+
         while (!stack.isEmpty()) {
-            int[] top = stack.pop();
-            answer[top[1]] = n - top[1] - 1;
+            Stock top = stack.pop();
+            answer[top.idx] = prices.length - top.idx - 1;
         }
-        
+
         return answer;
+    }
+
+    class Stock {
+        int price;
+        int idx;
+        Stock(int price, int idx) {
+            this.price = price;
+            this.idx = idx;
+        }
     }
 }
