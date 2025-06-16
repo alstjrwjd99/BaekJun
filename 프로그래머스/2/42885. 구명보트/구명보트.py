8076fadfa2c3c6b21416f24e43094da98c611356
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2025-06-16 13:40:12 +0900

    [level 2] Title: 구명보트, Time: 9.11 ms, Memory: 9.52 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,11 +1,11 @@
 def solution(people, limit):
-    answer = 0
-    i = 0
-    N = len(people) - 1
     people.sort()
-    while (i <= N) :
-        if people[i] + people[N] <= limit :
-            i += 1
+    left, right = 0, len(people) - 1
+    answer = 0
+
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            left += 1
+        right -= 1
         answer += 1
-        N -= 1 
-    return (answer)
+    return answer
