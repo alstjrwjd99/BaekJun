12fcf3c67935f02a7c6835a6bb18fcb8009e485f
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2025-06-16 11:16:39 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.49 ms, Memory: 9.78 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,10 +1,10 @@
 def solution(s):
-    answer = [0] * len(s)
-    sets = {i : 0 for i in set(s)}
-    for index,alpha in enumerate (s):
-        if sets[alpha] == 0:
-            answer[index] = -1
+    answer = []
+    str_idx = dict()
+    for idx,alpha in enumerate(s) :
+        if alpha in str_idx :
+            answer.append(idx - str_idx[alpha])
         else :
-            answer[index] = index+1 - sets[alpha]
-        sets[alpha] = index+1
+            answer.append(-1)
+        str_idx[alpha] = idx
     return answer
