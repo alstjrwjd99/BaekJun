d29fae43b98de7fe2e17dc4e46c2c147a70570a7
codeZzankunst <42230162+alstjrwjd99@users.noreply.github.com>
2025-06-16 13:57:18 +0900

    [level 2] Title: [1차] 캐시, Time: 132.82 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,15 +1,58 @@
-from collections import deque
+'''
+hash map -> city : node
+링크드 리스트 사용
+'''
+class Node :
+    def __init__(self,key) :
+        self.key = key
+        self.prev = None
+        self.next = None
 
+class LRUCache :
+    def __init__(self,capacity):
+        self.capacity = capacity
+        self.cache = {}
+        self.time = 0
+
+        self.head = Node(0)
+        self.tail = Node(0)
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def add_front_tail(self, node):
+        prev = self.tail.prev
+        prev.next = node
+        node.prev = prev
+        node.next = self.tail
+
+        self.tail.prev = node
+
+    def put(self, key):
+        if key in self.cache :
+            node = self.cache[key]
+            self.remove(node)
+            self.time += 1
+        else : self.time += 5
+
+        node = Node(key)
+        self.cache[key] = node
+        self.add_front_tail(node)
+
+        if len(self.cache) > self.capacity:
+            lru = self.head.next
+            self.remove(lru)
+            del self.cache[lru.key]
+    
+    def get_time(self):
+        return self.time
+        
 def solution(cacheSize, cities):
-    answer = 0
-    queue = deque(maxlen = cacheSize)
-    for city in cities:
-        city = city.lower()
-        if city in queue:
-            queue.remove(city)
-            queue.append(city)
-            answer += 1
-        else :
-            queue.append(city)
-            answer += 5        
-    return answer
+    lru = LRUCache(cacheSize)
+    for city in cities :
+        lru.put(city.lower())
+    return lru.get_time()
